82c8e0cf600da045261e73b4e1a52e3b35f18dca
anyasristy <148508255+anyasristy@users.noreply.github.com>
2025-10-19 11:58:11 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import {useGame} from "./GameContext";
+import Field from "./Field";
+import Scoreboard from "./Scoreboard";
+import Welcome from "./Welcome";
 export default function App() {
-  return <></>;
+  const { playing } = useGame();
+  return (
+    <><h1>Whack a Mole Game</h1>
+      {playing?(<>
+          <Scoreboard/>
+          <Field/>
+        </>
+      ):(
+        <Welcome/>
+      )}</>
+  );
 }
